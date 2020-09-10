4fa93957a95501c8b0208dd497e5979009a64367
Талашманов Павел <qlouz1@yandex.ru>
2020-09-10 13:23:04 +0300

    create cart component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,40 @@ import React from 'react';
 import data from './data.json';
 import Products from './components/Products';
 import Filter from './components/Filter';
+import Cart from './components/Cart';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: '',
       sort: '',
     };
   }
+
+  addToCart = product => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
+
+  removeFromCart = item => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter(x => x._id !== item._id),
+    });
+  };
 
   sortProducts = e => {
     const sort = e.target.value;
@@ -34,6 +58,7 @@ class App extends React.Component {
         ),
     }));
   };
+
   filterProducts = e => {
     if (e.target.value === '') {
       this.setState({
@@ -66,9 +91,17 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               ></Filter>
-              <Products products={this.state.products} />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
         <footer>All rights are reserved</footer>
